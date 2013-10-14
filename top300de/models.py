e494dfb9111acd33c90a8b23a0dac7d0dfdf1e91
claudiu <claudiu.garba@gmail.com>
2013-10-14 18:11:44 +0200

    add polls and choice for models

--- top300de/models.py
+++ top300de/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Poll(models.Model):
+    question = models.CharField(max_length=100)
+    pub_date = models.DateField('date published')
+
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
